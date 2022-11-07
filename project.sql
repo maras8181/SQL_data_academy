2194470f84f824bfc58dbc82743fe93171fbb990
maras8181 <mrazekmartin81@centrum.cz>
2022-11-07 19:01:01 +0100

    second commit

--- project.sql
+++ project.sql
@@ -1,1 +1,43 @@
-SELECT * FROM czechia_price cp;
+CREATE OR REPLACE TABLE t_martin_mrazek_project_SQL_primary_final AS 
+(SELECT 
+	cpri.id AS cprice_id,
+	cpri.value AS cprice_value,
+	cpri.category_code,
+	cpri.date_from,
+	cpri.date_to,
+	cpri.region_code,
+	cpc.code AS cpc_code, 
+	cpc.name AS cpc_name,
+	cpc.price_value,
+	cpc.price_unit,
+	cpay.id AS cpayroll_id,
+	cpay.value AS cpayroll_value,
+	cpay.value_type_code,
+	cpay.unit_code,
+	cpay.calculation_code,
+	cpay.industry_branch_code,
+	cpay.payroll_year,
+	cpay.payroll_quarter,
+	cpib.code AS cpib_code,
+	cpib.name AS cpib_name
+FROM czechia_price cpri
+JOIN czechia_price_category cpc
+	ON cpri.category_code = cpc.code 
+JOIN czechia_payroll cpay 
+	ON YEAR(cpri.date_from) = cpay.payroll_year
+JOIN czechia_payroll_industry_branch cpib 
+	ON cpay.industry_branch_code = cpib.code)
+
+CREATE OR REPLACE INDEX i_category_code ON t_martin_mrazek_project_SQL_primary_final(category_code);
+CREATE OR REPLACE INDEX i_date_from ON t_martin_mrazek_project_SQL_primary_final(date_from);
+
+SELECT 
+	DISTINCT tmm.category_code ,
+	tmm.cpc_name,
+	round(avg(tmm.cprice_value), 2),
+	tmm.date_from,
+	tmm.date_to 
+FROM t_martin_mrazek_project_sql_primary_final tmm
+WHERE value_type_code = 5958
+GROUP BY tmm.category_code, year(tmm.date_from), month(tmm.date_from);
+
