48f2ad5efcbf3fc64a2e3bd5d3ca315bc1ddc5cd
maras8181 <mrazekmartin81@centrum.cz>
2022-11-08 22:35:56 +0100

    the first and third questions are done

--- project.sql
+++ project.sql
@@ -33,10 +33,77 @@ WHERE cpay.value_type_code = 5958);
 
 CREATE OR REPLACE INDEX i_category_code ON t_martin_mrazek_project_SQL_primary_final(category_code);
 CREATE OR REPLACE INDEX i_date_from_month ON t_martin_mrazek_project_SQL_primary_final(entry_month); 
-CREATE OR REPLACE INDEX i_date_from_month ON t_martin_mrazek_project_SQL_primary_final(entry_year); 
+CREATE OR REPLACE INDEX i_date_from_year ON t_martin_mrazek_project_SQL_primary_final(entry_year); 
+CREATE OR REPLACE INDEX i_industry_branch_code ON t_martin_mrazek_project_SQL_primary_final(industry_branch_code); 
+CREATE OR REPLACE INDEX i_payroll_year ON t_martin_mrazek_project_SQL_primary_final(payroll_year); 
+CREATE OR REPLACE INDEX i_payroll_quarter ON t_martin_mrazek_project_SQL_primary_final(payroll_quarter);
 
 /*
- * Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční 
+ * 1. Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají? 
+ */
+
+CREATE OR REPLACE VIEW v_martin_mrazek_task_1 AS (
+SELECT 
+	DISTINCT tmm.cpayroll_id,
+	tmm.industry_branch_code,
+	tmm.cpib_name,
+	tmm.cpayroll_value,
+	tmm.payroll_year,
+	tmm.payroll_quarter
+FROM t_martin_mrazek_project_sql_primary_final tmm
+ORDER BY tmm.industry_branch_code, tmm.payroll_year);
+
+SELECT 
+	rt3.industry_branch_code,
+	rt3.cpib_name AS name,
+	rt3.avg_wages_in_period_2006_2012,
+	rt3.avg_wages_in_period_2012_2018,
+	CASE 
+		WHEN rt3.avg_wages_in_period_2006_2012 < rt3.avg_wages_in_period_2012_2018
+			THEN "The wages are raising"
+		ELSE "The wages are falling"
+	END AS result_of_wages	
+FROM 
+	(SELECT 
+		rt2.industry_branch_code,
+		lag(rt2.industry_branch_code) 
+			OVER (ORDER BY rt2.industry_branch_code, rt2.time_range) AS industry_branch_code_prev_row,
+		rt2.cpib_name,
+		round(avg(rt2.avg_value), 2) AS avg_wages_in_period_2012_2018,
+		lag(round(avg(rt2.avg_value), 2)) 
+			OVER (ORDER BY rt2.industry_branch_code, rt2.time_range) AS avg_wages_in_period_2006_2012
+	FROM 
+		(SELECT 
+			*,
+			CASE 
+				WHEN rt1.industry_branch_code = rt1.industry_branch_code_prev_row
+					THEN rt1.avg_value_in_year_quarter
+				ELSE 0
+			END AS avg_value,
+			CASE 
+				WHEN rt1.payroll_year < 2012 THEN "2006-2012"
+				WHEN rt1.payroll_year = 2012 AND rt1.payroll_quarter IN (1,2) THEN "2006-2012"
+				ELSE "2012-2018"
+			END AS time_range
+		FROM 
+			(SELECT 
+				vmmt1.industry_branch_code,
+				lag(vmmt1.industry_branch_code)
+					OVER (ORDER BY vmmt1.industry_branch_code, vmmt1.payroll_year, vmmt1.payroll_quarter) AS industry_branch_code_prev_row,
+				vmmt1.cpib_name,
+				avg(vmmt1.cpayroll_value) AS avg_value_in_year_quarter,
+				lag(avg(vmmt1.cpayroll_value)) 
+					OVER (ORDER BY vmmt1.industry_branch_code, vmmt1.payroll_year, vmmt1.payroll_quarter) AS avg_value_in_year_quarter_prev_row,
+				vmmt1.payroll_year,
+				vmmt1.payroll_quarter
+			FROM v_martin_mrazek_task_1 vmmt1
+		GROUP BY vmmt1.industry_branch_code, vmmt1.payroll_year, vmmt1.payroll_quarter) AS rt1) AS rt2
+		GROUP BY rt2.industry_branch_code, rt2.time_range) AS rt3
+	WHERE rt3.industry_branch_code = rt3.industry_branch_code_prev_row;
+
+
+/*
+ * 3. Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční 
  * nárůst)?
  */
 
